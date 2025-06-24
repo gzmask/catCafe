0a768de4e37b8b984507e5a9324b09818c0994ef
ray-lei <r.lei@sap.com>
2025-06-23 22:49:48 -0700

    add NPC Maia

--- src/catcafe/core.clj
+++ src/catcafe/core.clj
@@ -25,6 +25,10 @@
 (def ^:const player-visual-width 121.5)
 (def ^:const player-visual-height 175.5)
 (def ^:const player-collision-size 81)
+(def ^:const npc-speed 100)
+(def ^:const npc-visual-width 121.5)
+(def ^:const npc-visual-height 175.5)
+(def ^:const npc-direction-change-interval 3.0) ;; Change direction every 3 seconds
 (def game-state (atom nil))
 
 
@@ -34,7 +38,14 @@
    :player-y 100
    :animation-time 0
    :is-moving false
-   :facing-right true})
+   :facing-right true
+   ;; Maia NPC state
+   :maia-x 200
+   :maia-y 100
+   :maia-animation-time 0
+   :maia-facing-right true
+   :maia-direction-timer 0
+   :maia-moving-right true})
 
 
 (defn create-walking-animation-right
@@ -50,6 +61,26 @@
   []
   (let [walking1 (Texture. "images/ysabelWalkingRight1.png")
         walking2 (Texture. "images/ysabelWalkingRight2.png")
+        region1 (TextureRegion. walking1)
+        region2 (TextureRegion. walking2)
+        _ (.flip region1 true false)
+        _ (.flip region2 true false)
+        frames (into-array TextureRegion [region1 region2])]
+    (Animation. (float animation-frame-duration) frames)))
+
+(defn create-maia-walking-animation-right
+  []
+  (let [walking1 (Texture. "images/maiaWalkingRight1.png")
+        walking2 (Texture. "images/maiaWalkingRight2.png")
+        frames (into-array TextureRegion
+                           [(TextureRegion. walking1)
+                            (TextureRegion. walking2)])]
+    (Animation. (float animation-frame-duration) frames)))
+
+(defn create-maia-walking-animation-left
+  []
+  (let [walking1 (Texture. "images/maiaWalkingRight1.png")
+        walking2 (Texture. "images/maiaWalkingRight2.png")
         region1 (TextureRegion. walking1)
         region2 (TextureRegion. walking2)
         _ (.flip region1 true false)
@@ -85,6 +116,18 @@
             walking-animation-right (create-walking-animation-right)
             walking-animation-left (create-walking-animation-left)
 
+            ;; Create Maia standing textures and sprites
+            maia-standing-texture (Texture. "images/maiaWalkingRight1.png")
+            maia-standing-sprite-right (doto (Sprite. maia-standing-texture)
+                                         (.setSize npc-visual-width npc-visual-height))
+            maia-standing-sprite-left (doto (Sprite. maia-standing-texture)
+                                        (.setSize npc-visual-width npc-visual-height)
+                                        (.flip true false))
+
+            ;; Create Maia walking animations
+            maia-walking-animation-right (create-maia-walking-animation-right)
+            maia-walking-animation-left (create-maia-walking-animation-left)
+
             hallway-bg-texture (Texture. "images/hallway_bg.png")]
         (.setToOrtho camera false 800 600)
         (reset! game-state
@@ -95,6 +138,10 @@
                        :standing-sprite-left standing-sprite-left
                        :walking-animation-right walking-animation-right
                        :walking-animation-left walking-animation-left
+                       :maia-standing-sprite-right maia-standing-sprite-right
+                       :maia-standing-sprite-left maia-standing-sprite-left
+                       :maia-walking-animation-right maia-walking-animation-right
+                       :maia-walking-animation-left maia-walking-animation-left
                        :hallway-bg-texture hallway-bg-texture))))
 
     (render
@@ -146,6 +193,29 @@
                  :is-moving is-moving
                  :facing-right facing-right))
 
+        ;; Update Maia NPC
+        (let [{:keys [maia-x maia-y maia-direction-timer maia-moving-right maia-animation-time]} @game-state
+              new-maia-direction-timer (+ maia-direction-timer delta)
+              
+              ;; Change direction periodically
+              [new-maia-moving-right new-timer] (if (>= new-maia-direction-timer npc-direction-change-interval)
+                                                  [(not maia-moving-right) 0]
+                                                  [maia-moving-right new-maia-direction-timer])
+              
+              ;; Calculate new Maia position
+              new-maia-x (if new-maia-moving-right
+                           (min (- floor-x-max player-collision-size) (+ maia-x (* npc-speed delta)))
+                           (max floor-x-min (- maia-x (* npc-speed delta))))
+              
+              new-maia-animation-time (+ maia-animation-time delta)]
+          
+          (swap! game-state assoc
+                 :maia-x new-maia-x
+                 :maia-direction-timer new-timer
+                 :maia-moving-right new-maia-moving-right
+                 :maia-facing-right new-maia-moving-right
+                 :maia-animation-time new-maia-animation-time))
+
         ;; Draw
         (.begin batch)
         ;; Draw background first
@@ -153,7 +223,22 @@
                (float 0) (float 0)
                (float 800) (float 600))  ;; Use the window size
 
-        ;; Draw player
+        ;; Draw Maia NPC (before player so player appears in front)
+        (let [{:keys [maia-x maia-y maia-facing-right maia-animation-time
+                      maia-walking-animation-right maia-walking-animation-left
+                      maia-standing-sprite-right maia-standing-sprite-left]} @game-state
+              maia-current-animation (if maia-facing-right
+                                       maia-walking-animation-right
+                                       maia-walking-animation-left)
+              maia-current-standing (if maia-facing-right
+                                      maia-standing-sprite-right
+                                      maia-standing-sprite-left)
+              maia-current-frame (.getKeyFrame maia-current-animation maia-animation-time true)]
+          (.draw batch maia-current-frame
+                 (float maia-x) (float maia-y)
+                 (float npc-visual-width) (float npc-visual-height)))
+
+        ;; Draw player (after Maia so player appears in front)
         (let [{:keys [is-moving facing-right]} @game-state
               current-animation (if facing-right
                                   walking-animation-right
@@ -170,12 +255,14 @@
             (do
               (.setPosition current-standing (float player-x) (float player-y))
               (.draw current-standing batch))))
+
         (.end batch)))
 
     (dispose
       []
       (let [{:keys [batch standing-sprite-right
                     walking-animation-right walking-animation-left
+                    maia-standing-sprite-right maia-walking-animation-right maia-walking-animation-left
                     hallway-bg-texture]} @game-state]
         (.dispose batch)
         (.dispose (.getTexture standing-sprite-right))
@@ -184,5 +271,10 @@
         (doseq [frame (.getKeyFrames walking-animation-right)]
           (.dispose (.getTexture frame)))
         ;; No need to dispose walking-animation-left frames as they use the same textures
+
+        ;; Dispose Maia textures
+        (.dispose (.getTexture maia-standing-sprite-right))
+        (doseq [frame (.getKeyFrames maia-walking-animation-right)]
+          (.dispose (.getTexture frame)))
 
         (.dispose hallway-bg-texture)))))
